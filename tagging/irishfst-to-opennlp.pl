8ad3e7d2cf2509ff8cb97ff2628355b270466ec0
Jim O'Regan <jaoregan@tcd.ie>
2017-08-26 18:27:34 +0100

    convert

---
+++ tagging/irishfst-to-opennlp.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+BEGIN{
+print '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+print "<dictionary>\n";
+}
+
+my %forms = ();
+while(<>) {
+    chomp;
+    next if(/\+Event/);
+    next if(/\+XMLTag/);
+    my @line = split /\t/;
+    my $text = $line[0];
+    $text =~ s/\+/\t/;
+    $text =~ s/\+/:/g;
+    my @nline = split/\t/, $text;
+    if(exists $forms{$line[1]}) {
+        $forms{$line[1]} .= " $nline[1]";
+    } else {
+        $forms{$line[1]} = $nline[1];
+    }
+}
+
+for my $k (keys %forms) {
+    print "<entry tags=\"$forms{$k}\">\n<token>$k</token>\n</entry>\n";
+}
+
+END{
+print '<dictionary>';
+}

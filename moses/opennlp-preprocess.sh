43dac4da7b3c6be48265c7d1ef9d974a52d7babe
Jim O'Regan <jaoregan@tcd.ie>
2017-09-04 13:23:14 +0100

    add script

---
+++ moses/opennlp-preprocess.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OPENNLP_PATH=/tmp/apache-opennlp-1.8.1
+SCRDIR=$(dirname "$0")
+OPENNLP_MODELS=$SCRDIR
+OPENNLP=$OPENNLP_PATH/bin/opennlp
+MOSESDIR=/home/jim/Playing/mosesdecoder
+
+if [ $1 = "en" ]
+then
+	TOK=$OPENNLP_MODELS/en-token.bin
+	POS=$OPENNLP_MODELS/en-pos-maxent.bin
+	LWR="perl $MOSESDIR/scripts/tokenizer/lowercase.perl"
+else
+	TOK=$OPENNLP_MODELS/ga-token.bin.zip
+	POS=$OPENNLP_MODELS/ga-pos-maxent.zip
+	LWR=$SCRDIR/tolower.sh
+fi
+
+$OPENNLP TokenizerME $TOK | $LWR | perl scripts/tokenizer/replace-unicode-punctuation.perl
+
